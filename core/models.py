c2dd21cb69a89546563df1c95f4e4aab33738f07
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2023-12-12 07:21:23 +0900

    Add ActiveManager class model manager

--- core/models.py
+++ core/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class ActiveManager(models.Manager):
+    """Model manager that retrieves active items
+
+    This class defines a new default query set so the project can always filter data that is active
+    """
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
 class CommonInfo(models.Model):
     """CommonInfo model class
 
@@ -12,6 +21,9 @@ class CommonInfo(models.Model):
 
     # This allows me to escape to default django query set if I need it later
     all_objects = models.Manager()
+
+    # for active query set
+    objects = ActiveManager()
 
     class Meta:
         abstract = True
