906326591aa4eaa1b367ccb64afacedd71133f91
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2023-12-12 07:03:06 +0900

    Add initial models

--- messenger/models.py
+++ messenger/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ChannelUser(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ChannelMessage(models.Model):
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    sender = models.ForeignKey(ChannelUser, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.channel.name} - {self.sender.name}'
