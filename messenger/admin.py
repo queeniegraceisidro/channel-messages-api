af816b63bd3c8fa02f807c7012a3cd54e86716f7
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2023-12-12 07:04:48 +0900

    Add initial django admin classes

--- messenger/admin.py
+++ messenger/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from messenger.models import Channel, ChannelUser, ChannelMessage
 
-# Register your models here.
+
+class ChannelAdmin(admin.ModelAdmin):
+    search_fields = ["id", "name"]
+    list_display = ["id", "name", "deleted_at"]
+    list_filter = ["deleted_at"]
+
+
+class ChannelUserAdmin(admin.ModelAdmin):
+    search_fields = ["id", "name"]
+    list_display = ["id", "name", "deleted_at"]
+    list_filter = ["deleted_at"]
+
+
+class ChannelMessageAdmin(admin.ModelAdmin):
+    search_fields = ["id", "name"]
+    list_display = ["id", "channel", "sender", "message"]
+    autocomplete_fields = ['channel', 'sender']
+
+
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(ChannelUser, ChannelUserAdmin)
+admin.site.register(ChannelMessage, ChannelMessageAdmin)
