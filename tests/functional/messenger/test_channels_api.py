74ec3ea9aac7f179c955b0da2de07c4d4d567d2b
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-01-10 07:01:05 +0900

    Create testcase for patch endpoint

--- tests/functional/messenger/test_channels_api.py
+++ tests/functional/messenger/test_channels_api.py
@@ -24,3 +24,30 @@ class TestChannelViewSet:
         assert response.status_code == http_client.CREATED
         assert response["content-type"] == "application/json"
         assert response.data['name'] == name
+
+    def test_patch(self, client):
+        # Arrange
+        prev_name = 'new test channel'
+        old_data = {'name': prev_name}
+
+        # Act
+        response = client.post(self.endpoint, old_data)
+
+        # Assert
+        assert response.status_code == http_client.CREATED
+        assert response["content-type"] == "application/json"
+        assert response.data['name'] == prev_name
+
+        # Arrange
+        patch_id = response.data['id']
+        patch_endpoint = f'{self.endpoint}{patch_id}/'
+        new_name = 'changed test channel name'
+        new_data = {'name': new_name}
+
+        # Act
+        response = client.patch(patch_endpoint, new_data)
+
+        # Assert
+        assert response.status_code == http_client.OK
+        assert response["content-type"] == "application/json"
+        assert response.data['name'] == new_name
