f01522bbfb8055cd1257aab0e1b006d281af658a
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-02-20 14:19:07 +0900

    Add JWTAPIClient class
    - This class inherits rest_framework's APIClient but added a new function for authenticating a user used by our testcases

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,8 +1,23 @@
 import pytest
 
 from rest_framework.test import APIClient
+from django.urls import reverse
 
 
 @pytest.fixture
 def client():
-    return APIClient()
+    return JWTAPIClient()
+
+
+class JWTAPIClient(APIClient):
+
+    def authenticate_user(self, username, password):
+        login_url = reverse("rest_login")
+
+        login_data = {
+            "username": username,
+            "password": password,
+        }
+
+        # Login using JWT because the client's force_authenticate function doesn't log out the user
+        return self.post(login_url, login_data)
