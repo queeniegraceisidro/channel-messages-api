a964b45f706ecd65534660f243f9aae6158b2b4e
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-05-24 11:40:43 +0900

    Modify Django UserManager
    - Change required field from email to username
    - Fix trimmed_email function error when there's no email provided
    - Set User's email field as nullable

--- users/managers.py
+++ users/managers.py
@@ -6,33 +6,33 @@ class UserManager(BaseUserManager):
     Extends Django's base user manager
     """
 
-    def create_user(self, email, password=None, **kwargs):
+    def create_user(self, username, password=None, **kwargs):
         """
         Creates a normal user
-        :param email: email string
+        :param username: username string
         :param password: password string
         :param kwargs: other parameters to be saved
         :return: user
         """
         # Create a normal user
-        if not email:
-            raise ValueError("Email is required.")
+        if not username:
+            raise ValueError("Username is required.")
 
-        user = self.model(email=email, **kwargs)
+        user = self.model(username=username, **kwargs)
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, email, password=None, **kwargs):
+    def create_superuser(self, username, password=None, **kwargs):
         """
         Creates a superuser
-        :param email: email string
+        :param username: username string
         :param password: password string
         :param kwargs: other parameters to be saved
         :return: user
         """
-        user = self.create_user(email, password, **kwargs)
+        user = self.create_user(username, password, **kwargs)
         user.is_superuser = True
         user.is_staff = True
         user.save()
