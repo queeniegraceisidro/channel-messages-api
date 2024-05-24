7ff790508b38156cedc86da34042450a0b742839
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-05-24 11:50:11 +0900

    Create view for User registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from dj_rest_auth.registration.views import RegisterView
+from rest_framework import permissions
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+
+class UserRegisterView(RegisterView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
