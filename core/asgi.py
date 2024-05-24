43f08bb2417af761e123e2c57ad8ee79e39cc969
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-05-24 11:12:05 +0900

    Add protocoltype router
    - The router accomodates the websocket later down in the commits

--- core/asgi.py
+++ core/asgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+    }
+)
