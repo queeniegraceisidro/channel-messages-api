31a52671356691ae356d96fb8988ef414bda059a
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-05-24 16:24:30 +0900

    Create a new message consumer
    - Create a message consumer that pings all the users in the message group
    - Create a BaseConsumer that inherits the AsyncWebsocketConsumer and contains methods that is shared with the two consumer classes

--- messenger/consumers.py
+++ messenger/consumers.py
@@ -37,49 +37,68 @@ def is_channel_member_async(channel_id, user_id) -> Channel | None:
     return result
 
 
-class ChannelConsumer(AsyncWebsocketConsumer):
-
+class BaseConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
-        self.channel_id = None
         self.name = ""
         self.user = None
-        self.messenger_channel: Channel | None = None
 
     def _get_user(self):
         return self.scope["user"]
 
-    def _get_channel_id(self):
-        return self.scope["url_route"]["kwargs"]["channel_id"]
-
     async def connect(self):
-        await self.accept()
+        self.user = self._get_user()
+
+        if self.user.is_anonymous:
+            await self.accept()
+            await self.send_error("User is not authenticated")
+            await self.close(401)
+            await self.disconnect(401)
+
+    async def close(self, code=None):
+        if code is not None and code is not True:
+            await super().send({"type": "websocket.close", "code": code})
+        else:
+            await super().send({"type": "websocket.close"})
+
+    async def send_error(self, message):
+        await self.send(text_data=json.dumps({"error": message}))
 
     async def disconnect(self, close_code):
         # Leave room group
         if self.name:
             await self.channel_layer.group_discard(self.name, self.channel_name)
 
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        self.user = self._get_user()
+
+class ChannelConsumer(BaseConsumer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.channel_id = None
+        self.messenger_channel: Channel | None = None
+
+    def _get_channel_id(self):
+        return self.scope["url_route"]["kwargs"]["channel_id"]
+
+    async def connect(self):
+        await super().connect()
         self.channel_id = self._get_channel_id()
-
-        if self.user.is_anonymous:
-            await self.send_error("User is not authenticated")
-            await self.disconnect(401)
-
-        if not is_channel_member_async(self.channel_id, self.user.id):
-            # Only members of this channel can access this resource
-            await self.send_error("User is not a member of this channel")
-            await self.disconnect(403)
-
         self.messenger_channel = get_channel_async(self.channel_id)
         self.name = self.messenger_channel.messenger_name()
 
+        if not is_channel_member_async(self.channel_id, self.user.id):
+            # Only members of this channel can access this resource
+            await self.accept()
+            await self.send_error("User is not a member of this channel")
+            await self.close(403)
+            await self.disconnect(403)
+
         # Join channel group
         await self.channel_layer.group_add(self.name, self.channel_name)
+        await self.accept()
 
+    # Receive message from WebSocket
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
@@ -94,5 +113,30 @@ class ChannelConsumer(AsyncWebsocketConsumer):
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
 
-    async def send_error(self, message):
-        await self.send(text_data=json.dumps({"error": message}))
+
+class ChannelMemberConsumer(BaseConsumer):
+
+    async def connect(self):
+        await super().connect()
+
+        self.name = "channel-members"
+
+        # Join channel group
+        await self.channel_layer.group_add(self.name, self.channel_name)
+        await self.accept()
+
+    # Receive message from WebSocket
+    async def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to the users
+        await self.channel_layer.group_send(
+            self.name, {"type": "new_user_message", "message": message}
+        )
+
+    # Sent the message to all the users
+    async def new_user_message(self, event):
+        message = event["message"]
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
